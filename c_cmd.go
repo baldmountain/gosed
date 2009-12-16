826278c5f26e45f942af7dc2d70c461aa570ac74
Geoffrey Clements <baldmountain@gmail.com>
2009-12-15 20:07:50 -0500

    Make c work a bit more like sed

--- c_cmd.go
+++ c_cmd.go
@@ -43,35 +43,35 @@ func (c *c_cmd) match(line []byte, lineNumber int) bool {
 func (c *c_cmd) String() string {
 	if c != nil {
 		if c.addr != nil {
-			return fmt.Sprintf("{Append Cmd addr:%s text:%s}", c.addr.String(), c.text)
+			return fmt.Sprintf("{c command addr:%s text:%s}", c.addr.String(), c.text)
 		}
-		return fmt.Sprintf("{Append Cmd text:%s}", c.text)
+		return fmt.Sprintf("{c command text:%s}", c.text)
 	}
-	return fmt.Sprintf("{Append Cmd}")
+	return fmt.Sprintf("{c command}")
 }
 
 func (c *c_cmd) printText(s *Sed) {
 	// we are going to get the newline from the
-	fmt.Fprint(s.outputFile, string(c.text))
+	s.outputFile.Write(c.text)
 }
 
 func (c *c_cmd) processLine(s *Sed) (bool, os.Error) {
-  s.patternSpace = s.patternSpace[0:0]
+	s.patternSpace = s.patternSpace[0:0]
 	if c.addr != nil {
-	  switch c.addr.address_type {
-	    case ADDRESS_RANGE:
-  	    if s.lineNumber+1 == c.addr.rangeEnd {
-					c.printText(s);
-					return true, nil
-  	    }
-	    case ADDRESS_LINE, ADDRESS_REGEX, ADDRESS_LAST_LINE:
-				c.printText(s);
+		switch c.addr.address_type {
+		case ADDRESS_RANGE:
+			if s.lineNumber+1 == c.addr.rangeEnd {
+				c.printText(s)
 				return true, nil
-	    case ADDRESS_TO_END_OF_FILE:
-	      // FIX need to output at end of file
-    }
+			}
+		case ADDRESS_LINE, ADDRESS_REGEX, ADDRESS_LAST_LINE:
+			c.printText(s)
+			return true, nil
+		case ADDRESS_TO_END_OF_FILE:
+			// FIX need to output at end of file
+		}
 	} else {
-		c.printText(s);
+		c.printText(s)
 		return true, nil
 	}
 	return false, nil
